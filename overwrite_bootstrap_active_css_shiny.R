4e3f7b6d6a194cd85d0cd606785c4064df1a5f03
amrrs <amrrs@outlook.com>
2017-10-13 18:10:13 +0530

    Create overwrite_bootstrap_active_css_shiny.R

---
+++ overwrite_bootstrap_active_css_shiny.R
@@ -0,0 +1,63 @@
+
+
+if(interactive()){
+  
+  library(shiny)
+   
+ UI =  shinyUI(fluidPage(theme = "bootstrap.css",
+                    tags$script("setInterval(function(){
+                                $('.active').removeClass('active');//remove class active
+},1000);"),
+                  (navbarPage("B Version",
+                              position = c("fixed-top"),
+                              fluid=TRUE,
+                              navbarMenu("Help", 
+                                         tabPanel(
+                                           a("Manual",
+                                             target="_blank", href="Manual.pdf")
+                                         ),
+                                         tabPanel(
+                                           a("Supporte",
+                                             target="_blank", href="gpl.pdf")
+                                         ),
+                                         tabPanel(
+                                           a("Tutorials",
+                                             downloadLink("AbE", "Expression", class=" fa fa-cloud-download"),
+                                             downloadLink("DiEx", "Expression", class=" fa fa-cloud-download")
+                                           )
+                                         )
+                              ),
+                              navbarMenu("Sample Data",
+                                         tabPanel(
+                                           downloadLink("AData", " Aff", class=" fa fa-cloud-download")
+                                         ),
+                                         tabPanel(
+                                           downloadLink("CData", " Code", class=" fa fa-cloud-download")
+                                         ),
+                                         tabPanel(
+                                           downloadLink("IData", " Il", class=" fa fa-cloud-download")
+                                         )
+                              ),
+                              navbarMenu("Stand-Alone Version",
+                                         tabPanel(
+                                           downloadLink("CodeandData", " app", class=" fa fa-cloud-download")
+                                         ),
+                                         tabPanel(
+                                           a("Stand-alone Manual",
+                                             target = "_blank", href= "Stand-alone.pdf")
+                                         )
+                              )
+                              
+  )
+  )
+  )
+  )
+
+ SERVER = shinyServer(function(input, output,session) {
+   
+ })
+ 
+ shinyApp(ui = UI, server = SERVER)
+    
+}
+
