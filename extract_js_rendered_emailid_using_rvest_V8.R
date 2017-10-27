b4905c48cbf3cf75be23f1592d03916d7dd73fff
amrrs <amrrs@outlook.com>
2017-10-27 20:44:41 +0530

    missed v8()

--- extract_js_rendered_emailid_using_rvest_V8.R
+++ extract_js_rendered_emailid_using_rvest_V8.R
@@ -2,13 +2,11 @@ library(rvest)
 library(V8)
 
 link <- 'https://food.list.co.uk/place/22191-brewhemia-edinburgh/'
+
 page <- read_html(link)
-name_html <- html_nodes(page,'.placeHeading')
-business_adr <- html_text(adr_html)
-tel_html <- html_nodes(page,'.value')
-business_tel <- html_text(tel_html)
 
-emailjs <- page %>% html_nodes('li') %>% html_nodes('script') %>% html_text()
+emailjs <- read_html(link) %>% html_nodes('li') %>% html_nodes('script') %>% html_text()
+
+ct <- v8()
+
 read_html(ct$eval(gsub('document.write','',emailjs))) %>% html_text()
-
-
