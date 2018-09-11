c60cad31e328f5fb6340385e1a0523a9560367e6
amrrs <amrrs@outlook.com>
2018-09-11 10:02:03 +0530

    Create shiny_DT_cell_color.R

---
+++ shiny_DT_cell_color.R
@@ -0,0 +1,33 @@
+library(shiny)
+library(tidyverse)
+library(DT)
+
+shinyApp(
+  ui = fluidPage(
+    
+    sidebarLayout(
+      sidebarPanel(
+        fluidRow(  
+          column(8,dataTableOutput("view"))
+        )
+      ),
+      
+      mainPanel(
+      ))),
+  
+  server = function(input, output, session){
+    
+    correct <- reactiveValues(num = 7)
+    wrong <- reactiveValues(num = 4)   
+    skipped <- reactiveValues(num = 9)
+    
+    togo = 80
+    
+    output$view <- renderDataTable(datatable(tibble(
+      Right = correct$num,
+      Wrong = wrong$num,
+      Skipped = skipped$num,
+      ToGo = togo
+    )) %>% formatStyle("Right",color=styleEqual(7, "red")) ) 
+  }
+)
